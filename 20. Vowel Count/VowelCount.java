0ac29b63eeedf1afb1b05df79f0366c70aef7a55
John-Francis Murphy <jfmurphy411@gmail.com>
2015-11-02 12:28:40 +0000

    Initial Commit

---
+++ 20. Vowel Count/VowelCount.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class VowelCount 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		
+		String line;
+		//int vowelCount = 0;
+		
+		for (int i = 0; i < count; i++)
+		{
+			int vowelCount = 0;
+			line = sc.nextLine();
+			int lineLength = line.length();
+			
+			for (int charIndex = 0; charIndex < lineLength; charIndex++)
+			{	
+				switch(line.charAt(charIndex))
+				{
+				case 'a':
+					vowelCount++;
+					break;
+				case 'e':
+					vowelCount++;
+					break;
+				case 'i':
+					vowelCount++;
+					break;
+				case 'o':
+					vowelCount++;
+					break;
+				case 'u':
+					vowelCount++;
+					break;
+				case 'y':
+					vowelCount++;
+					break;
+				}
+			}
+			
+			System.out.print(vowelCount + " ");
+		}
+	}
+
+}
