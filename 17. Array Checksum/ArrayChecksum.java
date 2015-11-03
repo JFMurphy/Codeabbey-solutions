ddfb74f370630e051d39b65f2919b870ab4c68c2
John-Francis Murphy <jfmurphy411@gmail.com>
2015-11-03 12:51:54 +0000

    Added Array Checksum

---
+++ 17. Array Checksum/ArrayChecksum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ArrayChecksum {
+	
+	public static void main (String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		final int SEED = 113;
+		final int LIMIT = 10000007;
+		
+		int count = sc.nextInt();
+		int result = 0;
+		
+		int[] values = new int[count];
+		
+		for (int i = 0; i < count; i++) {
+			values[i] = sc.nextInt();
+			
+			result = result + values[i];
+			result %= LIMIT;
+			result *= SEED;
+			result %= LIMIT;
+		}
+		
+		System.out.println(result);
+	}
+}
