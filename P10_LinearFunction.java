9f4c582c112acc457a2a4ae43f927037cc01b2ef
John-Francis Murphy <jfmurphy411@gmail.com>
2015-12-10 15:33:49 +0000

    Added previously solved problems

---
+++ P10_LinearFunction.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class P10_LinearFunction {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int count;
+		count = sc.nextInt();
+		int a = 0;			// gradient
+		int b = 0;			// y intercept
+		
+		for (int i = 0; i < count; i++) {
+			int x1 = 0;
+			int y1 = 0;
+			int x2 = 0;
+			int y2 = 0;
+			
+			x1 = sc.nextInt();
+			y1 = sc.nextInt();
+			x2 = sc.nextInt();
+			y2 = sc.nextInt();	
+			
+			a = (y1 - y2) / (x1 - x2);
+			
+			b = y1 - (a * x1);
+			
+			System.out.println("(" + a + " " + b + ") ");
+		}		
+	}
+}
---
+++ P19_MatchingBrackets.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class P19_MatchingBrackets {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		boolean roundOpen = false;
+		boolean squareOpen = false;
+		boolean curlyOpen = false;
+		boolean angleOpen = false;
+		
+		for (int i = 0; i < count; i++) {
+			String line = sc.nextLine();
+			
+			for (int j = 0; j < line.length(); j++) {
+				
+				switch(line.charAt(j)) {
+				case '(':
+					int openBracket = j;
+					int closeBracket = line.indexOf(')');
+					
+					if (closeBracket > -1)
+					
+					break;
+					
+				case '[':
+					
+					break;
+					
+				case '{':
+					
+					break;
+					
+				case '<':
+					
+					break;
+				}
+			}
+		}
+	}
+	
+	private void checkRoundBracket(String line, int startPoint, int endPoint) {
+		
+	}
+	
+	private void checkSquareBracket(String line, int startPoint, int endPoint) {
+		String insideBrackets = line.substring(startPoint+1, endPoint);
+		
+		for (int i = 0; i < insideBrackets.length(); i++) {
+			int sOpenBracket = i;
+			int sCloseBracket = insideBrackets.indexOf(']');
+			
+		}
+	}
+	
+	private void checkCurlyBracket(String line, int startPoint, int endPoint) {
+		
+	}
+	
+	private void checkAngleBracket(String line, int startPoint, int endPoint) {
+		
+	}
+}
