f07f820c9ecfbaa7d1f2e454d9d3c4195c35962c
John-Francis Murphy <jfmurphy411@gmail.com>
2015-12-14 12:23:02 +0000

    Completed Nuemanns Random Generator

---
+++ P24_NuemannsRandomGenerator.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P24_NuemannsRandomGenerator {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int count = sc.nextInt();
+
+		for (int i = 0; i < count; i++) {
+			ArrayList<Integer> nums = new ArrayList<Integer>();
+			int value = sc.nextInt();
+			int iterations = 0;
+
+			while (true) {
+
+				nums.add(value);
+
+				value *= value;
+				// Truncating
+				value /= 100;
+				value %= 10000;
+
+				iterations++;
+
+				if (nums.contains(value)) {
+					break;
+				}
+			}
+			System.out.print(iterations + " ");
+		}
+
+		sc.close();
+	}
+
+}
