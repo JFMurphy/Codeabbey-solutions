0f6df30cae44dedcd1b7e054feb5479837993174
John-Francis Murphy <jfmurphy411@gmail.com>
2016-02-25 16:27:24 +0000

    Added problem descriptions in each file.

--- Python/P003SumsInLoop.py
+++ Python/P003SumsInLoop.py
@@ -1,3 +1,25 @@
+""" 
+ If you already learned how to write program with a simple loop from Sum in Loop task, this new exercise will be just a simple modification.
+
+Now we are given several pairs of values and we want to calculate sum for each pair.
+
+Input data will contain the total count of pairs to process in the first line.
+The following lines will contain pairs themselves - one pair at each line.
+Answer should contain the results separated by spaces.
+
+Example:
+
+data:
+3
+100 8
+15 245
+1945 54
+
+answer:
+108 260 1999
+
+"""
+
 count = int(raw_input())
 
 for couple in range(0, count):
--- Python/P007FahrenheitToCelsius.py
+++ Python/P007FahrenheitToCelsius.py
@@ -1,3 +1,26 @@
+"""
+This programming exercise is roughly the same as counting sums in loop, but it needs bit more calculations.
+
+Note: the problem Rounding explains the rounding algorithm which is used in this task.
+
+There are two widespread systems of measuring temperature - Celsius and Fahrenheit. First is quite popular in Europe and second is well in use in United States for example.
+
+By Celsius scale water freezes at 0 degrees and boils at 100 degrees. By Fahrenheit water freezes at 32 degrees and boils at 212 degrees. You may learn more from wikipedia on Fahrenheit. Use these two points for conversion of other temperatures.
+
+You are to write program to convert degrees of Fahrenheit to Celsius.
+
+Input data contains N+1 values, first of them is N itself (Note that you should not try to convert it).
+Answer should contain exactly N results, rounded to nearest integer and separated by spaces.
+
+Example:
+
+data:
+5 495 353 168 -39 22
+answer:
+257 178 76 -39 -6
+Please note that first 5 is not a temperature, but the amount of values to convert!
+"""
+
 data = raw_input().split()
 data.pop(0)
 data = [float(i) for i in data]
